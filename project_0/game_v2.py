b2f49d4d781e01be6e0f1b1701a1c3e0e18820ca
Olesya92 <msshabanovalesya@yandex.ru>
2022-11-10 15:34:05 +0300

    add tests

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -2,45 +2,49 @@
 import numpy as np
 
 def random_predict(number:int=1) -> int:
-    """Рандомно угадываем число
+    """Randomly giess a number
 
     Args:
-        number (int, optional): Загаданное число. Defaults to 1.
+        number (int, optional): Hidden number. Defaults to 1.
 
     Returns:
-        int: Число попыток
+        int: Number of attemps
     """
 
     count = 0
-
+    start = 1
+    end = 100
     while True:
+
         count += 1
-        predict_number = np.random.randint(1, 101) # предполагаемое число
+        predict_number = (start+end) // 2 # метод бинарного поиска
+        if predict_number == start:
+            return(count) 
         if number == predict_number:
             break # выход из цикла, если угадали
+        elif number < predict_number:
+            end = predict_number
+        else:
+            start = predict_number     
     return(count)
 
-print(f'Количество попыток: {random_predict()}')
-
 def score_game(random_predict) -> int:
-    """За какое количество попыток в среднем из 1000 подходов угадывает наш алгоритм
+    """For how many attempts on average out of 1000 approaches does our algorithm guess
 
     Args:
-        random_predict ([type]): функция угадывания
+        random_predict ([type]): guessing function
 
     Returns:
-        int: среднее количество попыток
+        int: average number of attempts
     """
 
     count_ls = [] # список для сохранения количества попыток
     np.random.seed(1) # фиксируем сид для воспроизводимости
     random_array = np.random.randint(1, 101, size=(1000)) # загадали список чисел
-
     for number in random_array:
-        count_ls.append(random_predict(number))
-
+        count_ls.append(random_predict(number))   
     score = int(np.mean(count_ls)) # находим среднее количество попыток
-
+    
     print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
     return(score)
 #RUN
